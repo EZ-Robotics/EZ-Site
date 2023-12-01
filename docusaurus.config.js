80b14f1de2c164fdc058e6484a2f3700ec79f947
Jess Zarchi <jackfz01@gmail.com>
2023-12-01 00:39:34 -0800

    Updated url to .github.io

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,11 +15,10 @@ const config = {
   tagline: 'robotics is ez',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ez-robotics.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/EZ-Site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
