c2cb806459373a85f7c3c98b611a0d53bb7d8682
Jess Zarchi <jackfz01@gmail.com>
2025-11-29 12:29:27 -0800

    Updated packages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
   projectName: 'EZ-Site', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  //onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
